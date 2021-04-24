c61c4f2b6982da8985bbd3b9f63a144a7ca09dec
Klismann Barros <klismannbarros@eng.ci.ufpb.br>
2021-04-24 11:44:02 -0300

    Added images to use, styles and components to the application

---
+++ podcastrnext/src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import '../styles/global.scss';
+
+import Header from '../components/Header';
+import styles from '../styles/app.module.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  )
+}
+
+export default MyApp
